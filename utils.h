9f74346f5e6b5e1e789f91e260ebce874c9ac567
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2016-02-28 08:48:25 -0500

    Adding Random choice function.

--- utils.h
+++ utils.h
@@ -1,9 +1,26 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
+#include <cstdlib>
+
 #include "CImg.h"
 #include "Sift.h"
 
 cimg_library::CImg<double> paste_image(const cimg_library::CImg<double>&, const cimg_library::CImg<double>&);
+
+
+/* Reference: http://stackoverflow.com/a/6943003/227884 */
+template <typename I>
+I random_element(I begin, I end)
+{
+    const unsigned long n = std::distance(begin, end);
+    const unsigned long divisor = (RAND_MAX + 1) / n;
+
+    unsigned long k;
+    do { k = std::rand() / divisor; } while (k >= n);
+
+    std::advance(begin, k);
+    return begin;
+}
 
 #endif
