0234be7ac03d51711b6e6c509fe46f63be346a37
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2016-02-27 23:52:29 -0500

    Removed functions; they have been moved SiftMatcher.

--- utils.cpp
+++ utils.cpp
@@ -30,38 +30,4 @@ CImg<double> paste_image(const CImg<double>& input1, const CImg<double>& input2)
 	return result;
 }
 
-double descriptor_distance(const SiftDescriptor& s1, const SiftDescriptor& s2) {
-	double sum = 0;
-	for(int k = 0; k < 128; k++) {
-		sum += std::pow((s1.descriptor[k]-s2.descriptor[k]),2);
-	}
-	return std::sqrt(sum);
-}
-
-
-cimg_library::CImg<double> annotate_sift_points(const CImg<double>& image, const std::vector<SiftDescriptor>& descriptors) {
-	CImg<double> result(image);
-	for(int i=0; i<descriptors.size(); i++) {
-		for(int j=0; j<5; j++) {
-			for(int k=0; k<5; k++) {
-				if(j==2 || k==2) {
-					for(int p=0; p<3; p++) {
-						if(descriptors[i].col+k < result.width() && descriptors[i].row+j < result.height()) {
-							result(descriptors[i].col+k, descriptors[i].row+j, 0, p)=0;
-						}
-					}
-				}
-			}
-		}
-	}
-	return result;
-}
-
-std::ostream& operator<<(std::ostream& out, const SiftDescriptor& desc) {
-	out<<"Descriptor: x=" << desc.col << " y=" << desc.row << " descriptor=(";
-	for(int l=0; l<128; l++)
-		out<<desc.descriptor[l]<<",";
-	out<<")"<<std::endl;
-	return out;
-}
 
--- utils.h
+++ utils.h
@@ -6,8 +6,4 @@
 
 cimg_library::CImg<double> paste_image(const cimg_library::CImg<double>&, const cimg_library::CImg<double>&);
 
-cimg_library::CImg<double> annotate_sift_points(const CImg<double>& image, const std::vector<SiftDescriptor>&);
-
-double descriptor_distance(const SiftDescriptor&, const SiftDescriptor&);
-
 #endif
